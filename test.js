11ef6ae486c0f497454f4baba1eb65ea203ab30e
samooth <tomas@bsv.direct>
2022-08-02 23:34:28 +0200

    Added node test

--- test.js
+++ test.js
@@ -1,69 +1,50 @@
 const Bitails = require('./src/index.js')
 
 const mainAddress = "16Rcy7RYM3xkPEJr4tvUtL485Fuobi8S7o"
+const mainScriptHash = "0c03b02d68a3590137b2cd8eed603009c809de8f5f0c72dded16e49f377c71e0"
+
 const testAddress = "mhcSJ3eF5f2Bf7mVQfceXXeLNQMBF3vqCv"
 
 
-function networkMain(ex, cb){
-	let status = ex.status().then((estado) => {
-	console.log("Estado de Red")
-    console.log(estado);
-   	ex.chainInfo().then((info)=>{
-   		console.log(info)
-   		ex.blockHeight(0).then((genesis)=>{
-   			console.log("Hash Bloque Genesis:",genesis.hash);
-   			ex.blockLatest().then((ultimo)=>{
-   				console.log("Último bloque:", ultimo.hash);
-   				console.log("Número de transacciones: ",ultimo.transactionsDetails.length)
-   			})
-   		})
-   	})
-
-	}).then(()=>{
-		cb(ex, mainAddress)
-	})
-}
-
-function addressRoutine(ex, address, cb){
-	 console.log("Balance of "+address)
-	 ex.balance(address).then(function(b) {
-        console.log(b);
-				 ex.utxos(address).then(function(utxos) {
-		        	 console.log("Mostrando "+utxos.unspent.length +"Utxos of "+address)
-
-			        console.log(Object.keys(utxos) )
-			        console.table(utxos.unspent)
-			        	 ex.history(address).then(function(historial) {
-			        	 	console.log("Historial de "+address)
-			        		console.log(Object.keys(historial) )
-			        		
-			        		console.log(historial.history.length+" Resultados del historial:");
-					        console.table(historial.history)
-					        cb
-
-			   			 });
-			    });
-
-
-    });
-}
-
-function testnet(testex){
-	 addressRoutine(testex, testAddress, process.exit(0))
-
-}
 
 const mainex = new Bitails("main");
 const testex = new Bitails("test");
 
-testnet(testex)
-networkMain(mainex, addressRoutine)
-/*
-{
-  address: '16Rcy7RYM3xkPEJr4tvUtL485Fuobi8S7o',
-  scripthash: '51268a643b2d4569263086daf88fa1e0b50c8530894842108891299750da7072',
-  confirmed: 543516275,
-  unconfirmed: 236964,
-  summary: 543753239,
-  count: 1987921
-}*/
+
+	mainex.mempoolInfo().then((memInfo)=>{
+		console.log(memInfo)
+	})
+	mainex.mempoolTxs().then((memTxs)=>{
+		console.log(memTxs)
+	})
+
+	mainex.addressInfo("18t6VHyC8gWqj5LjQcKoLN8v9JPY83HwYd").then((addrInfo)=>{
+		console.log(addrInfo)
+	})
+	mainex.balance("18t6VHyC8gWqj5LjQcKoLN8v9JPY83HwYd").then((balance)=>{
+		console.log(balance)
+	})
+
+	mainex.history("18t6VHyC8gWqj5LjQcKoLN8v9JPY83HwYd").then((historial)=>{
+		console.log(historial)
+	})
+
+	mainex.utxos(mainAddress).then((utxos)=>{
+		console.log(utxos)
+	})
+
+	mainex.detailsScriptHash(mainScriptHash).then((detailsScriptHash)=>{
+		console.log(detailsScriptHash)
+	})
+
+	mainex.historyByScriptHash(mainScriptHash).then((scriptHashHistorial)=>{
+		console.log(scriptHashHistorial)
+	})
+
+	mainex.utxosByScriptHash(mainScriptHash).then((utxosByScriptHash)=>{
+		console.log(utxosByScriptHash)
+	})
+	
+	mainex.balanceScriptHash(mainScriptHash).then((balanceSH)=>{
+		console.log(balanceSH)
+	})
